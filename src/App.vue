3edc897102cf73f8633986b1a923834a110afb7d
jasoncheng <shenjingwa444@qq.com>
2022-03-05 23:51:56 +0800

    ä»添加404页面

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,11 @@
   <div id="app">
     <router-view/>
     <hr/>
-    <div>
-    <router-link to="/money">记账</router-link>
-      |
-    <router-link to="/labels">标签</router-link>
-      |
-    <router-link to="/statistics">统计</router-link>
 <!--    <div id="nav">-->
 <!--      <router-link to="/">Home</router-link> |-->
 <!--      <router-link to="/about">About</router-link>-->
 <!--    </div>-->
 <!--    <router-view/>-->
-    </div>
   </div>
 </template>
 
@@ -39,3 +32,9 @@
   }
 }
 </style>
+<script>
+import Nav from "@/components/Nav"
+export default {
+  components: {Nav}
+}
+</script>
