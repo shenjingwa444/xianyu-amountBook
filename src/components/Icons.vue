5f0b6c9954ba64b6dcfa23426d96cf56f4b8ea9a
jasoncheng <shenjingwa444@qq.com>
2022-03-06 16:47:28 +0800

    引入Icon组件

---
+++ src/components/Icons.vue
@@ -0,0 +1,34 @@
+<template>
+  <svg class="icon">
+    <use v-bind:xlink:href="'#'+name"/>
+  </svg>
+</template>
+
+<script lang="ts">
+//将icons里面的svg文件全部引入到body里面
+//__WebpackModuleApi 报错：undefined ，解决方法：在 eslintrc.js 加:
+// "globals":{
+//   "__WebpackModuleApi":"writable"
+// },
+let importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
+try {
+  importAll(require.context('../assets/icons', true, /\.svg$/));//使用require
+} catch (error) {
+  console.log(error);
+}
+export default {
+  //props的[] 的值要加引号
+  props: ['name'],
+  name: 'Icon'
+};
+</script>
+
+<style lang="scss" scoped>
+.icon {
+  width: 1em;
+  height: 1em;
+  vertical-align: -0.2em;
+  fill: currentColor;
+  overflow: hidden;
+}
+</style>
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,29 +1,21 @@
 <template>
   <div>
-    <svg>
-      <use xlink:href="#labels"/>
-      <router-link to="/money">记账</router-link>
-    </svg>
+    <Icon name="money_bag_fill"/>
+    <router-link to="/money">记账</router-link>
+    |
+    <Icon name="labels"/>
     <router-link to="/labels">标签</router-link>
     |
+    <Icon name="统计"/>
     <router-link to="/statistics">统计</router-link>
   </div>
 </template>
 
 <script lang="ts">
-//将icons里面的svg文件全部引入到body里面
-//__WebpackModuleApi 报错：undefined ，解决方法：在 eslintrc.js 加:
-// "globals":{
-//   "__WebpackModuleApi":"writable"
-// },
-let importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
-try {
-  importAll(require.context('../assets/icons', true, /\.svg$/));//使用require
-} catch (error) {
-  console.log(error);
-}
+import Icon from '@/components/Icons.vue';
+
 export default {
-  name: 'Icon'
+  components: {Icon}
 };
 </script>
 
