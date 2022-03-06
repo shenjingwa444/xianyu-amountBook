fa88e26745643744231c0453f0440e03e8d27aec
jasoncheng <shenjingwa444@qq.com>
2022-03-06 22:04:07 +0800

    定义CSS全局变量、reset、全局样式

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -24,6 +24,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "~@/assets/style/helper.scss";
 nav {
   display: flex;
   flex-direction: row;
@@ -44,7 +45,7 @@ nav {
     }
   }
   > .item.selected{
-    color:red;
+    color:$color-highlight;
   }
 }
 </style>
