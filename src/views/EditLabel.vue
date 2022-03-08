c75f17f283402a34fa9e7373f282e2021c98e5de
jasoncheng <shenjingwa444@qq.com>
2022-03-08 20:41:48 +0800

    新增 EditLabel 组件

---
+++ src/views/EditLabel.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>删除标签</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import {Component} from 'vue-property-decorator';
+import newTagModel from '@/models/newTagModel';
+
+@Component
+export default class EditLabel extends Vue {
+  created(){
+    const id = this.$route.params.id
+    newTagModel.fetch()
+    const tags = newTagModel.data
+    const tag = tags.filter(tag => tag.id === id)[0]
+    if(tag){
+      console.log(tag);
+    }else{
+      this.$router.replace('/404')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
