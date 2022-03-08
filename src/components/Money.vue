0d7c1c1ee8375d294afa684d3caecf611657eb17
jasoncheng <shenjingwa444@qq.com>
2022-03-08 15:59:36 +0800

    record 复制给 record2, localStorage 保存 recordList,添加日期、localStorage版本

--- src/components/Money.vue
+++ src/components/Money.vue
@@ -1,7 +1,7 @@
 <template>
   <Layout class-prefix="layout">
-    {{record }}
-    <NumberPad :value.sync="record.amount"/>
+    {{recordList }}
+    <NumberPad :value.sync="record.amount" @submit="saveRecord"/>
     <Types :value.sync="record.type"/>
     <Notes :value='record.notes' @update:value="onUpdateNotes"/>
     <Tags :data-source.sync="tags" @update:value="onUpdateTags"/>
@@ -14,7 +14,9 @@ import Tags from '@/components/Money/Tags.vue';
 import NumberPad from '@/components/Money/NumberPad.vue';
 import Types from '@/components/Money/Types.vue';
 import Notes from '@/components/Money/Notes.vue';
-import {Component} from 'vue-property-decorator';
+import {Component, Watch} from 'vue-property-decorator';
+
+window.localStorage.setItem('version','0.0.1')
 // ts 声明类型
 type Record = {
   //tags?:string[]  加问号之后，表示这个 Key 可以不要，有可能有，有可能没有
@@ -22,6 +24,7 @@ type Record = {
   type:string
   notes:string
   amount:number
+  createAt?:Date
 }
 @Component({
   components: {Notes, Types, NumberPad, Tags},
@@ -29,13 +32,24 @@ type Record = {
 export default class Money extends Vue {
   tags = ['衣', '食', '住', '行'];
   record:Record={
-    tags:[],type:'-',notes:'',amount:0
+    tags:[],type:'_',notes:'',amount:0
   }
+  recordList:Record[] =JSON.parse(window.localStorage.getItem('recordList') || '[]')
   onUpdateTags(value:string[]){
     this.record.tags = value
   }
   onUpdateNotes(value:string){
     this.record.notes = value
+  }
+  saveRecord(){
+    //record2 深拷贝 record ，record 是对象，传值，不能直接Push
+    const record2:Record = JSON.parse(JSON.stringify(this.record))
+    record2.createAt = new Date()
+    this.recordList.push(record2)
+  }
+  @Watch('recordList')
+  onRecordListChange(){
+    window.localStorage.setItem('recordList',JSON.stringify(this.recordList))
   }
   //当使用 .sync 时，value 会自动赋值给 record.type
   // onUpdateType(value:string){
