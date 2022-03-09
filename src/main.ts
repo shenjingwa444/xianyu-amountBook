9b7dccf13b710fd7cebdc1f6b0e66fafdd83ddc6
jasoncheng <shenjingwa444@qq.com>
2022-03-09 19:55:44 +0800

    封装 window.createTag

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,28 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import App from './App.vue';
+import './registerServiceWorker';
+import router from './router';
+import store from './store';
 import Nav from '@/components/Nav.vue';
 import Layout from '@/components/Layout.vue';
 import Icons from '@/components/Icons.vue';
 import newTagModel from '@/models/newTagModel';
 
-Vue.config.productionTip = false
-Vue.component('Nav',Nav)
-Vue.component('Layout',Layout)
-Vue.component('Icon',Icons)
-window.tagList = newTagModel.fetch()
-
+Vue.config.productionTip = false;
+Vue.component('Nav', Nav);
+Vue.component('Layout', Layout);
+Vue.component('Icon', Icons);
+window.tagList = newTagModel.fetch();
+window.createTag = (name: string) => {
+  const message = newTagModel.create(name);
+  if (message === 'duplicated') {
+    window.alert('标签名重复了');
+  } else if (message === 'success') {
+    window.alert('添加成功');
+  }
+};
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
