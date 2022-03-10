1d3f214b8fed834c0470513afb19456b103aff61
jasoncheng <shenjingwa444@qq.com>
2022-03-10 20:09:59 +0800

    Tags.vue 使用 Vuex

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -16,13 +16,21 @@
 <script lang="ts">
 import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
-import store from '@/store/index2';
+import store from '@/store/index';
 
-@Component
+@Component({
+  computed: {
+    tagList(){
+      return this.$store.state.tagList
+    }
+  }
+})
 export default class Tags extends Vue {
-  tagList = store.fetchTags();
   selectedTags: string[] = [];
 
+  created(){
+    this.$store.commit('fetchTags')
+  }
   toggle(tag: string) {
     const index = this.selectedTags.indexOf(tag);
     if (index < 0) {
@@ -35,8 +43,8 @@ export default class Tags extends Vue {
 
   create(tag: string) {
     const name = window.prompt('请输入标签名');
-    if (!name) { return window.alert('标签名不能为空')}
-    store.createTag(name);
+    if (!name) { return window.alert('标签名不能为空');}
+    this.$store.commit('createTag',name);
   }
 }
 </script>
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,30 +1,49 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import clone from '@/components/lib/clone';
+import createId from '@/components/lib/createId';
 
-Vue.use(Vuex)
-const localStorageKeyName = 'recordList';
+Vue.use(Vuex);
 
 const store = new Vuex.Store({
   state: {
     recordList: [] as RecordItem[],
+    tagList: [] as Tag[],
+
   },
   mutations: {
-    saveRecords(state){
-      window.localStorage.setItem(localStorageKeyName, JSON.stringify(state.recordList));
+    saveRecords(state) {
+      window.localStorage.setItem('recordList', JSON.stringify(state.recordList));
     },
     fetchRecords(state) {
-      state.recordList = JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]') as RecordItem[];
+      state.recordList = JSON.parse(window.localStorage.getItem('recordList') || '[]') as RecordItem[];
     },
-    createRecord(state,record){
+    createRecord(state, record) {
       //record2 深拷贝 record ，record 是对象，传值，不能直接Push
       const record2: RecordItem = clone(record);
       record2.createAt = new Date();
       state.recordList.push(record2);
-      console.log(state.recordList);
       store.commit('saveRecords');
     },
+    fetchTags(state) {
+      state.tagList = JSON.parse(window.localStorage.getItem('tagList') || '[]');
+    },
+    saveTags(state) {
+      window.localStorage.setItem('tagList', JSON.stringify(state.tagList));
+    },
+    createTag(state,name:string) {
+      const names = state.tagList.map(item => item.name);
+      if (names.indexOf(name) >= 0) {
+        window.alert('标签名重复了');
+        return 'duplicated';
+      }
+      const id = createId().toString();
+      state.tagList.push({id, name: name});
+      store.commit('saveTags');
+      window.alert('添加成功');
+      return 'success';
+    },
   },
-})
+});
 
-export default store
+export default store;
