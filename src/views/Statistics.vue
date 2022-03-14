d8a40b27b4ecb40ecca5523ec1a73a79a2badb6b
jasoncheng <shenjingwa444@qq.com>
2022-03-14 20:31:24 +0800

    重构 Statistics.vue 的 get()

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -2,7 +2,7 @@
   <Layout>
     <Tabs class-prefix="type" :data-source="recordTypeList" :value.sync="type"/>
     <div class="chart-wrapper" ref="chartWrapper">
-      <Chart :options="x" class="chart" ref="chart"/>
+      <Chart :options="chartOptions" class="chart" ref="chart"/>
     </div>
     <ol v-if="groupedList.length>0">
       <li v-for="(group,index) in groupedList" :key="index">
@@ -56,20 +56,20 @@ export default class Statistics extends Vue {
     return tags.length === 0 ? '无' : tags.map(t => t.name).join('，');
   }
 
-  get y() {
+  get keyValueList() {
     const today = new Date();
     const array = [];
     for (let i = 0; i <= 29; i++) {
       const dateString = dayjs(today).subtract(i, 'day').format('YYYY-MM-DD');
       const found = _.find(this.recordList, {createAt: dateString});
       array.push({
-        date: dateString, value: found ? found.amount : 0
+        key: dateString, value: found ? found.amount : 0
       });
     }
     array.sort((a, b) => {
-      if (a.date > b.date) {
+      if (a.key > b.key) {
         return 1;
-      } else if (a.date === b.date) {
+      } else if (a.key === b.key) {
         return 0;
       } else {
         return -1;
@@ -78,9 +78,9 @@ export default class Statistics extends Vue {
     return array
   }
 
-  get x() {
-    const keys = this.y.map(i => i.date);
-    const values = this.y.map(i => i.value);
+  get chartOptions() {
+    const keys = this.keyValueList.map(i => i.key);
+    const values = this.keyValueList.map(i => i.value);
     return {
       grid: {
         left: 0,
@@ -98,6 +98,11 @@ export default class Statistics extends Vue {
         axisTick: {
           alignWithLabel: true,
         },
+        axisLabel:{
+          formatter:function(value:string,index:number){
+            return value.substr(5);
+          }
+        }
 
       },
       yAxis: {
